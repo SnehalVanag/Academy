97cf7a83b8438086536aae63279005c4b86b895d
SnehalVanage <snehalvanage57@gmail.com>
2025-07-21 12:22:14 +0530

    initial commit

--- homepage1/settings.py
+++ homepage1/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'aadb',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
